df5d4ffb1f400c361ca6014ef03e399a7ccfef12
Mohan Lal <mhnuk2007@gmail.com>
2025-06-23 21:08:17 +0500

    Convert Car1 to abstract class and implement drive() in WagonR

--- src/AbstractKeywordDemo.java
+++ src/AbstractKeywordDemo.java
@@ -1,17 +1,24 @@
-class Car1 {
-    public void drive() {
-        // No implementation
-    }
+abstract class Car1 {
+    public abstract void drive(); // Declaration without implementation
+
 
     public void playMusic() {
         System.out.println("Music is playing");
     }
 }
 
+class WagonR extends Car1{
+
+    @Override
+    public void drive() {
+        System.out.println("Driving ...");
+    }
+}
+
 public class AbstractKeywordDemo {
     public static void main(String[] args) {
-        Car1 car = new Car1();
-        car.drive();         // does nothing
+        Car1 car = new WagonR();
+        car.drive();         // prints: Driving ...
         car.playMusic();     // prints: Music is playing
     }
 }

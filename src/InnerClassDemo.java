b9c299a9d775195f06c76d22a886edf9ab1c65d5
Mohan Lal <mhnuk2007@gmail.com>
2025-06-23 21:26:41 +0500

    Add example of static nested class with A2 and B

---
+++ src/InnerClassDemo.java
@@ -0,0 +1,23 @@
+class A2{
+    int age;
+
+    public void show() {
+        System.out.println("in A show");
+    }
+
+    static class B {
+        public void config() {
+            System.out.println("in B config");
+        }
+    }
+}
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        A2 obj = new A2();
+        obj.show();
+
+        A2.B obj1 = obj.new B();
+        obj1.config();
+    }
+}
